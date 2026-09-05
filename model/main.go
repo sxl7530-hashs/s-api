d663d48d6a3bf618a71f5bc91c579bc66958c9d3
sxl <sxl@sxldeMac-mini.local>
2026-09-06 00:14:27 +0800

    fix: normalize scanned json values

--- model/main.go
+++ model/main.go
@@ -27,6 +27,17 @@ var commonFalseVal string
 var logKeyCol string
 var logGroupCol string
 
+func jsonScanBytes(value interface{}) []byte {
+	switch v := value.(type) {
+	case []byte:
+		return v
+	case string:
+		return []byte(v)
+	default:
+		return nil
+	}
+}
+
 func initCol() {
 	// init common column names
 	if common.UsingMainDatabase(common.DatabaseTypePostgreSQL) {
